a24ca503fbc86048384fa9df598847e495ecd78b
Hirota Ryuki <kbc18b24@stu.kawahara.ac.jp>
2019-10-08 16:03:36 +0900

    肉の落下の実装 カメラ途中

---
+++ Assets/Hirota/Camera/CameraMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    //4箇所のカメラの位置
+    Vector3[] m_cameraPos = {
+        new Vector3(0.0f, -5.0f, 9.5f),
+        new Vector3(9.5f, -5.0f, 0.0f),
+        new Vector3(0.0f, -5.0f, -9.5f),
+        new Vector3(-9.5f, -5.0f, 0.0f),
+    };
+
+    //移動後の位置
+    Vector3 m_moveCameraPos = new Vector3(0.0f, 0.0f, 0.0f);
+
+    //移動するポジション番号
+    int m_cameraPosNumber = 0;
+
+    //ポジション番号の最小値
+    int m_min = 0;
+
+    //ポジション番号の最大値
+    int m_max = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //初期位置の設定
+        m_moveCameraPos = m_cameraPos[m_cameraPosNumber];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            if (m_cameraPosNumber == m_max)
+            {
+                m_cameraPosNumber = m_min;
+            }
+        }
+    }
+}
---
+++ Assets/Hirota/Meet/MeetGenerator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeetGenerator : MonoBehaviour
+{
+    //肉
+    public GameObject m_meet;
+    //肉焼き器の4辺の初期位置
+    Vector3[] m_nikuyaki = { 
+        new Vector3(0.0f, 5.0f, 4.0f),
+        new Vector3(4.0f, 5.0f, 0.0f),
+        new Vector3(0.0f, 5.0f, -4.0f),
+        new Vector3(-4.0f, 5.0f, 0.0f),
+    };
+    Vector3 m_meetPos = new Vector3( 0.0f, 0.0f, 0.0f );
+
+    //フレーム数
+    int m_cntTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //秒数の加算
+        m_cntTime++;
+
+        //肉焼き器の4辺から1辺をランダムで決める
+        //注：maxは含まれないので+1
+        int meetDirection = UnityEngine.Random.Range(0, 4);
+
+        //落ちる肉のポジションをセット
+        m_meetPos = m_nikuyaki[meetDirection];
+
+        //加減量をランダムで決める
+        float meetAdd = UnityEngine.Random.Range(2.5f, -2.5f);
+
+        //肉の横移動
+        if (meetDirection == 0 || meetDirection == 2)
+        {
+            m_meetPos.x += meetAdd;
+        }
+        else
+        {
+            m_meetPos.y += meetAdd;
+        }
+
+        
+        //もし20フレーム経ったら
+        if (m_cntTime > 20)
+        {
+            //肉を生成
+            Instantiate(m_meet, m_meetPos, Quaternion.identity);
+            //秒数をリセット
+            m_cntTime = 0;
+        }
+    }
+}
