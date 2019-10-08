28c009d4f4b29fe57851eaab26183a5ebc61f76b
ShanzhongXinzhijie <44658366+ShanzhongXinzhijie@users.noreply.github.com>
2019-10-08 14:23:21 +0900

    肉を取る(仮)

---
+++ Assets/Script/PlayerSprict.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSprict : MonoBehaviour
+{
+    public Camera playerCamera;//プレイヤーカメラ
+    public GameObject tangPoint;//掴んだ肉を配置する位置
+    public ScoreDrawer scoreManager;//スコアを管理するクラス
+
+    List<Rigidbody> grabRigidBodys = new List<Rigidbody>();//掴んでいる肉々
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.visible = false;//マウスカーソルを非表示に
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左クリックで取る
+        if (Input.GetMouseButton(0))
+        {
+            //肉たちとレイで判定
+            int layerMask = LayerMask.GetMask(new string[] { "NikuYasai" });
+            Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, layerMask))
+            {
+                //Debug.Log(hit.point);
+                grabRigidBodys.Add(hit.rigidbody);//リストに掴んだものを追加
+                scoreManager.AddLivingMeetCount();
+            }
+        }
+        else {
+            //肉を放す
+            grabRigidBodys.Clear();
+        }
+
+        foreach (Rigidbody body in grabRigidBodys)
+        {
+            body.position = tangPoint.transform.position;
+        }
+    }
+}
---
+++ Assets/Script/ScoreDrawer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDrawer : MonoBehaviour
+{
+    public Text livingText, deathText;
+
+    int livingMeet = 0;
+    int deathMeet = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void LateUpdate()
+    {
+        livingText.text = livingMeet.ToString() + "肉生存";
+        deathText.text = deathMeet.ToString() + "肉死亡";
+    }
+
+    /// <summary>
+    /// 肉カウントの増加
+    /// </summary>
+    public void AddLivingMeetCount()
+    {
+        livingMeet++;
+    }
+    public void AddDeathMeetCount()
+    {
+        deathMeet++;
+    }
+}
