78832daf2f45475cbbfa9d9b7b5eb08e5f8d7abf
Hirota Ryuki <kbc18b24@stu.kawahara.ac.jp>
2019-10-09 14:52:55 +0900

    制限時間とカウントダウンの追加

---
+++ Assets/Hirota/Time/StartCountDown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartCountDown : MonoBehaviour
+{
+    public Text m_timeText;
+    public static float time;
+    void Start()
+    {
+        time = 4;
+    }
+    void Update()
+    {
+        if (time > 1)
+        {
+            time -= Time.deltaTime;
+        }
+        int t = Mathf.FloorToInt(time);
+        m_timeText.text = t.ToString();
+
+        if (t == 0)
+        {
+            m_timeText.text = "始めろ";
+        }
+    }
+
+}
