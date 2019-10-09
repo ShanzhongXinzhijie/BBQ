448caab26b1fdff303c3c64023d4bc75131d2572
ShanzhongXinzhijie <44658366+ShanzhongXinzhijie@users.noreply.github.com>
2019-10-09 16:05:56 +0900

    ゲーム開始処理

--- Assets/Hirota/Time/StartCountDown.cs
+++ Assets/Hirota/Time/StartCountDown.cs
@@ -7,9 +7,14 @@ public class StartCountDown : MonoBehaviour
 {
     public Text m_timeText;
     public static float time;
+
+    public GameObject[] activeGameObject;//カウントダウン終了時に有効化するゲームオブジェクト
+    CountDown countDown;//制限時間カウントダウンのコンポーネント
+
     void Start()
     {
         time = 4;
+        countDown = GetComponent<CountDown>();
     }
     void Update()
     {
@@ -23,7 +28,27 @@ public class StartCountDown : MonoBehaviour
         if (t == 0)
         {
             m_timeText.text = "始めろ";
+            //コルーチンでゲーム開始
+            StartCoroutine("GameStart");
         }
     }
 
+    /// <summary>
+    /// 数秒後ゲーム開始(コルーチン)
+    /// </summary>
+    IEnumerator GameStart()
+    {
+        //1秒停止
+        yield return new WaitForSeconds(1);
+        //テキストを無効化
+        m_timeText.gameObject.SetActive(false);
+
+        //登録されているゲームオブジェクトを有効化
+        foreach (GameObject go in activeGameObject)
+        {
+            go.SetActive(true);
+        }
+        //制限時間カウントダウンを有効化
+        countDown.enabled = true;
+    }
 }
