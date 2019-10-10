f6908756db186f58aff258c5827a640c826485f8
Hirota Ryuki <kbc18b24@stu.kawahara.ac.jp>
2019-10-10 11:14:43 +0900

    肉の回転の追加 肉の落下速度の修正

--- Assets/Hirota/Meet/MeetGenerator.cs
+++ Assets/Hirota/Meet/MeetGenerator.cs
@@ -13,10 +13,15 @@ public class MeetGenerator : MonoBehaviour
         new Vector3(0.0f, 5.0f, -4.0f),
         new Vector3(-4.0f, 5.0f, 0.0f),
     };
+
+    //肉焼き器の4辺の初期位置
     Vector3 m_meetPos = new Vector3( 0.0f, 0.0f, 0.0f );
 
-    //フレーム数
-    int m_cntTime = 0;
+    //肉焼き器の4辺の初期角度
+    Vector3 m_meetRot = new Vector3( 0.0f, 0.0f, 0.0f );
+
+    //時間の計測
+    float m_cntTime = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +33,8 @@ public class MeetGenerator : MonoBehaviour
     void Update()
     {
         //秒数の加算
-        m_cntTime++;
+        m_cntTime += Time.deltaTime;
+        Debug.Log(m_cntTime);
 
         //肉焼き器の4辺から1辺をランダムで決める
         //注：maxは含まれないので+1
@@ -38,7 +44,7 @@ public class MeetGenerator : MonoBehaviour
         m_meetPos = m_nikuyaki[meetDirection];
 
         //加減量をランダムで決める
-        float meetAdd = UnityEngine.Random.Range(2.5f, -2.5f);
+        float meetAdd = UnityEngine.Random.Range(-2.5f, 2.5f);
 
         //肉の横移動
         if (meetDirection == 0 || meetDirection == 2)
@@ -50,14 +56,13 @@ public class MeetGenerator : MonoBehaviour
             m_meetPos.z += meetAdd;
         }
 
-        
-        //もし20フレーム経ったら
-        if (m_cntTime > 20)
+        //5秒経ったら
+        if (m_cntTime > 1.0f)
         {
             //肉を生成
-            Instantiate(m_meet, m_meetPos, Quaternion.identity);
+            Instantiate(m_meet, m_meetPos, Random.rotation);
             //秒数をリセット
-            m_cntTime = 0;
+            m_cntTime = 0.0f;
         }
     }
 }
